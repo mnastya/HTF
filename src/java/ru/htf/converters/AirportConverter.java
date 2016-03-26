91f83d25fd1c8f1885238af3bec7bc73df2852b6
mnastya <nastya.matrosova-94@yandex.ru>
2016-03-26 17:52:08 +0300

    first commit

---
+++ src/java/ru/htf/converters/AirportConverter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.htf.converters;
+
+import ru.htf.datatypes.Airport;
+import ru.htf.service.AirportService;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author chudin_e_v
+ */
+@FacesConverter("airportConverter")
+public class AirportConverter implements Converter {
+
+    public Object getAsObject(FacesContext fc, UIComponent uic, String value) {
+        if (value != null && value.trim().length() > 0) {
+            AirportService service = (AirportService) fc.getExternalContext().getApplicationMap().get("airportService");
+            for (Airport port : service.getAirports()) {
+                if (port.toString().equals(value)) {
+                    return port;
+                }
+            }
+        }
+        return null;
+    }
+
+    public String getAsString(FacesContext fc, UIComponent uic, Object object) {
+        if (object != null) {
+            return String.valueOf(((Airport) object).toString());
+        } else {
+            return null;
+        }
+    }
+}
